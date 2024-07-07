e58784a640e0f32175c415fb965c1d8835b5aa62
Al kafi <shakibuli08@gmail.com>
2024-07-07 12:57:17 +0600

    add post and blog management

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -48,6 +48,7 @@
                         <span class="menu-title">Dashboard</span>
                     </a>
                 </div>
+
                 <div class="menu-item">
                     <div class="menu-content pt-8 pb-2">
                         <span class="menu-section text-muted text-uppercase fs-8 ls-1">Content Management</span>
@@ -70,14 +71,6 @@
                         <span class="menu-arrow"></span>
                     </span>
                     <div class="menu-sub menu-sub-accordion  {{ Route::currentRouteName() == 'system' || Route::currentRouteName() == 'banner' || Route::currentRouteName() == 'aboutus' ? 'hover show' : '' }}">
-                        <a class="menu-item menu-accordion" href="{{route('banner')}}">
-                            <span class="menu-link {{ Route::currentRouteName() == 'banner' ? 'active' : '' }}">
-                                <span class="menu-bullet">
-                                    <span class="bullet bullet-dot"></span>
-                                </span>
-                                <span class="menu-title">Banner Content</span>
-                            </span>
-                        </a>
                         <a class="menu-item menu-accordion" href="{{route('system')}}">
                             <span class="menu-link {{ Route::currentRouteName() == 'system' ? 'active' : '' }}">
                                 <span class="menu-bullet">
@@ -86,12 +79,79 @@
                                 <span class="menu-title">System Content</span>
                             </span>
                         </a>
+                        <a class="menu-item menu-accordion" href="{{route('banner')}}">
+                            <span class="menu-link {{ Route::currentRouteName() == 'banner' ? 'active' : '' }}">
+                                <span class="menu-bullet">
+                                    <span class="bullet bullet-dot"></span>
+                                </span>
+                                <span class="menu-title">Banner Content</span>
+                            </span>
+                        </a>
+                        
                         <a class="menu-item menu-accordion" href="{{route('aboutus')}}">
                             <span class="menu-link {{ Route::currentRouteName() == 'aboutus' ? 'active' : '' }}">
                                 <span class="menu-bullet">
                                     <span class="bullet bullet-dot"></span>
                                 </span>
                                 <span class="menu-title">About Us Content</span>
+                            </span>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="menu-item">
+                    <div class="menu-content pt-8 pb-2">
+                        <span class="menu-section text-muted text-uppercase fs-8 ls-1">Post & Blog Management</span>
+                    </div>
+                </div>
+                <div data-kt-menu-trigger="click" class="menu-item menu-accordion {{ Route::currentRouteName() == 'system' || Route::currentRouteName() == 'banner' || Route::currentRouteName() == 'aboutus' ? 'hover show' : '' }}">
+                    <span class="menu-link">
+                        <span class="menu-icon">
+                            <!--begin::Svg Icon | path: icons/duotune/ecommerce/ecm007.svg-->
+                            <span class="svg-icon svg-icon-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                    <path d="M21 9V11C21 11.6 20.6 12 20 12H14V8H20C20.6 8 21 8.4 21 9ZM10 8H4C3.4 8 3 8.4 3 9V11C3 11.6 3.4 12 4 12H10V8Z" fill="black" />
+                                    <path d="M15 2C13.3 2 12 3.3 12 5V8H15C16.7 8 18 6.7 18 5C18 3.3 16.7 2 15 2Z" fill="black" />
+                                    <path opacity="0.3" d="M9 2C10.7 2 12 3.3 12 5V8H9C7.3 8 6 6.7 6 5C6 3.3 7.3 2 9 2ZM4 12V21C4 21.6 4.4 22 5 22H10V12H4ZM20 12V21C20 21.6 19.6 22 19 22H14V12H20Z" fill="black" />
+                                </svg>
+                            </span>
+                            <!--end::Svg Icon-->
+                        </span>
+                        <span class="menu-title">POst</span>
+                        <span class="menu-arrow"></span>
+                    </span>
+                    <div class="menu-sub menu-sub-accordion  {{ Route::currentRouteName() == 'system' || Route::currentRouteName() == 'banner' || Route::currentRouteName() == 'aboutus' ? 'hover show' : '' }}">
+                        <a class="menu-item menu-accordion" href="{{route('system')}}">
+                            <span class="menu-link {{ Route::currentRouteName() == 'system' ? 'active' : '' }}">
+                                <span class="menu-bullet">
+                                    <span class="bullet bullet-dot"></span>
+                                </span>
+                                <span class="menu-title">System Content</span>
+                            </span>
+                        </a>
+                        <a class="menu-item menu-accordion" href="{{route('banner')}}">
+                            <span class="menu-link {{ Route::currentRouteName() == 'banner' ? 'active' : '' }}">
+                                <span class="menu-bullet">
+                                    <span class="bullet bullet-dot"></span>
+                                </span>
+                                <span class="menu-title">Sub Category</span>
+                            </span>
+                        </a>
+                        
+                        <a class="menu-item menu-accordion" href="{{route('aboutus')}}">
+                            <span class="menu-link {{ Route::currentRouteName() == 'aboutus' ? 'active' : '' }}">
+                                <span class="menu-bullet">
+                                    <span class="bullet bullet-dot"></span>
+                                </span>
+                                <span class="menu-title">Post Create</span>
+                            </span>
+                        </a>
+                        <a class="menu-item menu-accordion" href="{{route('aboutus')}}">
+                            <span class="menu-link {{ Route::currentRouteName() == 'aboutus' ? 'active' : '' }}">
+                                <span class="menu-bullet">
+                                    <span class="bullet bullet-dot"></span>
+                                </span>
+                                <span class="menu-title">Post List</span>
                             </span>
                         </a>
                     </div>
