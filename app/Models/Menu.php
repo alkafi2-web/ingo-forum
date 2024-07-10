20c84fa2a81aaaa3eba0cf23eb701306954a5043
Md. Moniruzzaman (Office) <moon.mn717@gmail.com>
2024-07-10 11:12:06 +0600

    add page migration and update page model

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -8,4 +8,35 @@ use Illuminate\Database\Eloquent\Model;
 class Menu extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'has_sub_menu',
+        'type',
+        'page_id',
+        'route',
+        'url',
+        'media',
+        'content',
+        'position',
+        'visibility',
+    ];
+
+    /**
+     * Get the page associated with the menu.
+     */
+    public function page()
+    {
+        return $this->hasOne(Page::class, 'id', 'page_id');
+    }
+    
+    /**
+     * Check if the page has a menu.
+     *
+     * @return bool
+     */
+    public function hasMenu()
+    {
+        return $this->menu()->exists();
+    }
 }
