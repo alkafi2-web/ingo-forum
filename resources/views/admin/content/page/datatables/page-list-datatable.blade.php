f31997417f61c6e2fdfe4e012d428141c2f73c2e
Moniruzzaman (Office) <moon.mn717@gmail.com>
2024-07-11 12:05:07 +0600

    update datatable filter input

--- resources/views/admin/content/page/datatables/page-list-datatable.blade.php
+++ resources/views/admin/content/page/datatables/page-list-datatable.blade.php
@@ -42,7 +42,45 @@ $(document).ready(function() {
             { data: 'slug', name: 'slug' },
             { data: 'status', name: 'status', orderable: false, searchable: false },
             { data: 'actions', name: 'actions', orderable: false, searchable: false }
-        ]
+        ],
+        lengthMenu: [
+            [5, 10, 30, 50, -1],
+            [5, 10, 30, 50, "All"]
+        ], // Add 'All' option
+        pageLength: 5, // Set default page length
+        dom: "<'row'<'col-sm-4'l><'col-sm-4 d-flex justify-content-center'B><'col-sm-4'f>>" +
+            // Page length, buttons, and search
+            "<'row'<'col-sm-12'tr>>" + // Table rows
+            "<'row'<'col-sm-5'i><'col-sm-7'p>>", // Information and pagination
+        buttons: [{
+                extend: 'colvis',
+                columns: ':not(:first-child)' // Exclude first column (serial)
+            },
+            // 'excel', 'print', 'copy'
+        ],
+        language: {
+            search: '<div class="input-group">' +
+                '<span class="input-group-text">' +
+                '<i class="fas fa-search"></i>' +
+                '</span>' +
+                '_INPUT_' +
+                '</div>'
+        },
+        columnDefs: [{
+                targets: '_all',
+                searchable: true
+            },
+            {
+                targets: -1, // Target the last column (actions column)
+                className: 'text-center', // Optional: Center align the content in this column
+            },
+            {
+                targets: '_all',
+                searchable: true,
+                orderable: true
+            }
+        ],
+        // responsive: true,
     });
 });
 </script>
