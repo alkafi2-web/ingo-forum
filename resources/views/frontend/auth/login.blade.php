1a5237ad452007319a44f5e906909f8e86c78285
kafi <alkafirupam1@gmail.com>
2024-07-15 18:44:25 +0600

    add

--- resources/views/frontend/auth/login.blade.php
+++ resources/views/frontend/auth/login.blade.php
@@ -11,30 +11,28 @@
                     <h2 class="section-title text-center">Membership Login Form</h2>
                 </div>
                 <form id="member-form" action="" method="post">
-                    <div class="row pt-2">
+                    <div class="row pt-2 justify-content-center">
                         <!-- Member Access Credential Section -->
-                        <div class="col-12">
-                            <div class="row pt-2">
-                                <div class="col-lg-12 mb-3">
+                        <div class="col-12 ml-auto">
+                            <div class="row pt-2 justify-content-center">
+                                <div class="col-lg-8 mb-3">
                                     <div class="row align-items-center">
                                         <div class="col-md-4">
-                                            <label for="login_email" class="form-label required">Email (For
-                                                Login)</label>
+                                            <label for="login_email" class="form-label required">Email (For Login)</label>
                                         </div>
                                         <div class="col-md-8">
-                                            <input type="email" name="login_email" class="form-control"
-                                                id="login_email" placeholder="Email" required>
+                                            {{-- <label for="login_email" class="form-label required">Email (For Login)</label> --}}
+                                            <input type="email" name="login_email" class="form-control" id="login_email" placeholder="Email" required>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-lg-12 mb-3">
+                                <div class="col-lg-8 mb-3">
                                     <div class="row align-items-center">
                                         <div class="col-md-4">
                                             <label for="password" class="form-label required">Password</label>
                                         </div>
                                         <div class="col-md-8">
-                                            <input type="password" name="password" class="form-control" id="password"
-                                                placeholder="Password" required>
+                                            <input type="password" name="password" class="form-control" id="password" placeholder="Password" required>
                                         </div>
                                     </div>
                                 </div>
@@ -44,6 +42,7 @@
                             </div>
                         </div>
                     </div>
+                    
                 </form>
             </div>
         </div>
