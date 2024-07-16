568a7baf2c806bdee7932ba6c1bc405ba1f46583
kafi <alkafirupam1@gmail.com>
2024-07-16 15:58:57 +0600

    add approve and list

--- resources/views/admin/member/partials/view-header.blade.php
+++ resources/views/admin/member/partials/view-header.blade.php
@@ -1,5 +1,13 @@
 <div>
-    Organigation Name: <h4> {{ $member->memberInfos[0]['organisation_name'] }}</h4>
+    Organigation Name: <h4> {{ $member->memberInfos[0]['organisation_name'] }}  <i class="fas fa-check-circle text-success {{$member->status == 1 ? '' : 'd-none'}}"></i></h4>
+    
+
+    @if ($member->memberInfos[0]['membership_id'] != null)
+        <span class="membership-id ">
+            <i class="fas fa-id-badge fa-lg text-primary"></i>
+            {{ $member->memberInfos[0]['membership_id'] }}
+        </span>
+    @endif
 </div>
 <div>
     @if ($member->status != 3)
