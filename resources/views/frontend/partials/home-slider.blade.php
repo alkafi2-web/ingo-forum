4c38b601e61a6b56f07bdaa652412fb870471e21
Moniruzzaman (Office) <moon.mn717@gmail.com>
2024-07-30 16:53:08 +0600

    fixed slider issue

--- resources/views/frontend/partials/home-slider.blade.php
+++ resources/views/frontend/partials/home-slider.blade.php
@@ -1,25 +1,18 @@
 <!-- Hero Section Start  -->
 <section>
     <div class="owl-carousel owl-theme hero-slider">
-        @forelse ($global['banner'] as $banner)
-        @if($banner['overlay_color']['status'] ?? false)
-        <style>
-            .herobefore{{ $banner->id }}::before{
-                background-color: {{ $banner['overlay_color']['color'] }};
-            }
-            .cloned .item .herobefore{{ $banner->id }}::before{
-                background: none!important;
-            }
-        </style>
-        @endif
+        @forelse ($global['banner'] as $banner) 
         <div class="item">
-            <div class="hero-section herobefore{{ $banner->id }} ptb-70" 
+            <div class="hero-section ptb-70" 
                 @if($banner['background_color']['status'] ?? false) 
                     style="background-color: {{ $banner['background_color']['color'] }}"
                 @else 
                     style="background-image: url('{{ asset('public/frontend/images/banner/' . ($banner['bg_image']['path'] ?? 'default-bg.jpg')) }}')"
                 @endif
                 >
+                @if(!$banner['background_color']['status'] && ($banner['overlay_color']['status'] ?? false))
+                <div class="hero-overlay" style="background-color: {{ $banner['overlay_color']['color'] }}"></div>
+                @endif
                 <div class="container">
                     <div class="row d-flex align-items-center">
                         <div class="col-lg-6 hero-left mb-2 lg-mb-0">
@@ -46,7 +39,7 @@
         <!-- Default Banner -->
         <div class="item">
             <div class="hero-section ptb-70" style="background-image: url('{{ asset('public/frontend/images/banner/bg.png') }}'); background-color: #D7E8E0;">
-                <div class="hero-section::before" style="background-color: rgba(0, 0, 0, 0.5);"></div>
+                <div class="hero-overlay" style="background-color: rgba(0, 0, 0, 0.5);"></div>
                 <div class="container">
                     <div class="row d-flex align-items-center">
                         <div class="col-lg-6 hero-left mb-2 lg-mb-0">
